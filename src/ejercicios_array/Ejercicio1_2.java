eedc4f2bb03f0e61453f8d2b319ab1080c55b698
RYZEN <RYZEN@DESKTOP-PT59ORD>
2022-07-06 14:24:15 +0200

    Commit 1 version2

--- src/ejercicios_array/Ejercicio1_2.java
+++ src/ejercicios_array/Ejercicio1_2.java
@@ -1,18 +1,29 @@
 package ejercicios_array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Ejercicio1_2 {
 
     public static void main(String[] args) {
-        
-        String[] turno = {"M", "M", "M", "M", "T", "T", "T", "T"};
-        String[] nombre = {"Luis", "Miguel", "Carlos", "Carmen", "Arturo", "Gerson", "Delly", "María"};
-        double[] sueldo = {1000, 900, 800, 1210, 1000, 920, 850, 1500};
-        
-        List<String> turnos_al = new ArrayList<String>();
-        turnos_al.add("M");
+        List<String> turnos_al = new ArrayList<String>(Arrays.asList("M", "M", "M", "M", "T", "T", "T", "T"));
+        List<String> nombre_al = new ArrayList<String>(Arrays.asList("Luis", "Miguel", "Carlos", "Carmen", "Arturo", "Gerson", "Delly", "María"));
+        List<Double> sueldos_al = new ArrayList<Double>(Arrays.asList(1000.0, 900.0, 800.0, 1210.0, 1000.0, 920.0, 850.0, 1500.0));
+        double sueldom = 0;
+        double sueldot = 0;
+        for (int i = 0; i < turnos_al.size(); i++) {
+            String t = turnos_al.get(i);
+            double s = sueldos_al.get(i);
+            if (t.equalsIgnoreCase("M")) {
+                sueldom = sueldom + s;
+            }
+            if (t.equalsIgnoreCase("T")) {
+                sueldot = sueldot + s;
+            }
+        }
+        System.out.println("Gasto Sueldo Mañana: " + sueldom + "\u20ac");
+        System.out.println("Gasto Sueldo Tarde : " + sueldot + "\u20ac");
     }
 
 }
